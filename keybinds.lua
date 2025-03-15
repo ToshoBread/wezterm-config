41b495e5ea4a97957aa1e18e51e8d188db624ae3
Zion Dionisio <zndionisio.2121@gmail.com>
2025-03-16 02:15:04 +0800

    Updated wezterm config

--- keybinds.lua
+++ keybinds.lua
@@ -1,28 +1,53 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+
+local opacity = 0.85
+
 local keybinds = {
-	-- General keybinds
-	{ key = "c", mods = "CTRL", action = act.CopyTo("Clipboard") },
-	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+    -- General keybinds
+    { key = "c",   mods = "CTRL",       action = act.CopyTo("Clipboard") },
+    { key = "v",   mods = "CTRL",       action = act.PasteFrom("Clipboard") },
 
-	-- Multiplexing Keybinds
-	{ key = "m", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "m", mods = "CTRL|ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "-",   mods = "ALT",        action = act.DecreaseFontSize },
+    { key = "=",   mods = "ALT",        action = act.IncreaseFontSize },
 
-	{ key = "h", mods = "ALT", action = act.ActivatePaneDirection("Left") },
-	{ key = "l", mods = "ALT", action = act.ActivatePaneDirection("Right") },
-	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
-	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+    { key = "F11", mods = "NONE",       action = act.ToggleFullScreen },
 
-	{ key = "w", mods = "ALT", action = act.CloseCurrentPane({ confirm = true }) },
+    -- Multiplexing Keybinds
+    { key = "m",   mods = "ALT",        action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "m",   mods = "CTRL|ALT",   action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-	{ key = "h", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Left", 5 }) },
-	{ key = "l", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Right", 5 }) },
-	{ key = "k", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
-	{ key = "j", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
+    { key = "h",   mods = "ALT",        action = act.ActivatePaneDirection("Left") },
+    { key = "l",   mods = "ALT",        action = act.ActivatePaneDirection("Right") },
+    { key = "k",   mods = "ALT",        action = act.ActivatePaneDirection("Up") },
+    { key = "j",   mods = "ALT",        action = act.ActivatePaneDirection("Down") },
 
-	{ key = "-", mods = "ALT", action = act.DecreaseFontSize },
-	{ key = "=", mods = "ALT", action = act.IncreaseFontSize },
+    { key = "w",   mods = "ALT",        action = act.CloseCurrentPane({ confirm = true }) },
+
+    { key = "h",   mods = "CTRL|ALT",   action = act.AdjustPaneSize({ "Left", 5 }) },
+    { key = "l",   mods = "CTRL|ALT",   action = act.AdjustPaneSize({ "Right", 5 }) },
+    { key = "k",   mods = "CTRL|ALT",   action = act.AdjustPaneSize({ "Up", 5 }) },
+    { key = "j",   mods = "CTRL|ALT",   action = act.AdjustPaneSize({ "Down", 5 }) },
+
+    { key = "Y",   mods = "CTRL|SHIFT", action = wezterm.action.SendKey({ key = "Y", mods = "CTRL|SHIFT" }) },
+
+    { key = "Z",   mods = "CTRL|SHIFT", action = wezterm.action.SendKey({ key = "Z", mods = "CTRL|SHIFT" }) },
+    {
+        key = "Z",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action_callback(function(window, pane)
+            if opacity == 0.85 then
+                opacity = 1
+            else
+                opacity = 0.85
+            end
+            window:set_config_overrides({
+                window_background_opacity = opacity,
+            })
+        end),
+    },
+
+    { key = "Enter", mods = "ALT", action = wezterm.action.SendKey({ key = "Escape" }) },
 }
 
 return keybinds
--- wezterm.lua
+++ wezterm.lua
@@ -7,40 +7,51 @@ local keybinds = require("keybinds")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
 config = {
-	--Default Shell
-	default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" },
+    --Default Shell
+    default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" },
 
-	-- Custom keybinds
-	keys = { table.unpack(keybinds) },
-	disable_default_key_bindings = true,
+    -- Custom keybinds
+    keys = { table.unpack(keybinds) },
+    disable_default_key_bindings = true,
 
-	--Terminal Appearance
-	color_scheme = "Tokyo Night",
-	--font = wezterm.font("Jetbrains Mono", {}),
-	window_background_opacity = 0.95,
-	font_size = 12,
-	line_height = 1.3,
-	--default_cursor_style = "Line",
-	colors = {
-		cursor_bg = "white",
-		cursor_border = "white",
-	},
+    -- Terminal Behavior
+    term = "xterm-256color",
+    swallow_mouse_click_on_pane_focus = true,
 
-	--Window Appearance
-	window_decorations = "RESIZE", -- Removes the title bar but allows for window resizing
-	hide_tab_bar_if_only_one_tab = true,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+    -- Terminal Appearance
+    --color_scheme = "Tokyo Night",
+    color_scheme = "Deafened (terminal.sexy)",
+    -- font = wezterm.font("JetBrainsMonoNL NF Light"),
+    font = wezterm.font("Victor Mono"),
+    window_background_opacity = 0.85,
+    font_size = 12,
+    line_height = 1,
 
-	--Miscellaneous Settings
-	max_fps = 120,
-	prefer_egl = true,
+    -- Cursor Appearance
+    default_cursor_style = "BlinkingBar",
+    animation_fps = 1,
+    cursor_blink_ease_in = "Constant",
+    cursor_blink_ease_out = "Constant",
+    colors = {
+        cursor_bg = "white",
+        cursor_border = "white",
+    },
+
+    -- Window Appearance
+    window_decorations = "RESIZE", -- Removes the title bar but allows for window resizing
+    hide_tab_bar_if_only_one_tab = true,
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    },
+
+    -- Miscellaneous Settings
+    max_fps = 120,
+    prefer_egl = true,
+    enable_csi_u_key_encoding = true,
 }
--- and finally, return the configuration to wezterm
+
 return config
