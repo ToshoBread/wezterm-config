85907a7835e7dc3dceb7757d361bc69f913a69c0
Zion Dionisio <zndionisio.2121@gmail.com>
2025-05-10 17:53:13 +0800

    Added keybinds for tabs

--- keybinds.lua
+++ keybinds.lua
@@ -1,17 +1,25 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-local backgroundOpacity = 0.85
+local backgroundOpacity = 1
 
 local keybinds = {
-	-- General keybinds
-	{ key = "c", mods = "CTRL", action = act.CopyTo("Clipboard") },
+	-- General Keybinds
 	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
 
 	{ key = "-", mods = "ALT", action = act.DecreaseFontSize },
 	{ key = "=", mods = "ALT", action = act.IncreaseFontSize },
 
 	{ key = "F11", mods = "NONE", action = act.ToggleFullScreen },
+
+	-- Tab Keybinds
+	{ key = "t", mods = "ALT", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "ALT", action = act.CloseCurrentTab({ confirm = true }) },
+
+	{ key = "1", mods = "ALT", action = act.ActivateTab(0) },
+	{ key = "2", mods = "ALT", action = act.ActivateTab(1) },
+	{ key = "3", mods = "ALT", action = act.ActivateTab(2) },
+	{ key = "4", mods = "ALT", action = act.ActivateTab(3) },
 
 	-- Multiplexing Keybinds
 	{ key = "m", mods = "ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
@@ -22,20 +30,25 @@ local keybinds = {
 	{ key = "k", mods = "ALT", action = act.ActivatePaneDirection("Up") },
 	{ key = "j", mods = "ALT", action = act.ActivatePaneDirection("Down") },
 
-	{ key = "w", mods = "ALT", action = act.CloseCurrentPane({ confirm = true }) },
+	{ key = "w", mods = "CTRL|ALT", action = act.CloseCurrentPane({ confirm = true }) },
 
 	{ key = "h", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Left", 5 }) },
 	{ key = "l", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Right", 5 }) },
 	{ key = "k", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
 	{ key = "j", mods = "CTRL|ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
 
+	-- Misc Keybinds
 	{ key = "Y", mods = "CTRL|SHIFT", action = wezterm.action.SendKey({ key = "Y", mods = "CTRL|SHIFT" }) },
 
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action.SendString("\x1b[13;2u") },
+
 	{
-		key = "Z",
+		key = "Space",
 		mods = "CTRL|SHIFT",
 		action = wezterm.action_callback(function(window, pane)
-			backgroundOpacity = (backgroundOpacity == 0.85) and 1 or 0.85
+			local transclucent = 0.9
+			local opaque = 1
+			backgroundOpacity = (backgroundOpacity <= transclucent) and opaque or transclucent
 			window:set_config_overrides({
 				window_background_opacity = backgroundOpacity,
 			})
--- wezterm.lua
+++ wezterm.lua
@@ -17,33 +17,35 @@ config = {
 
 	-- Terminal Behavior
 	term = "xterm-256color",
-	swallow_mouse_click_on_pane_focus = true,
+	-- swallow_mouse_click_on_pane_focus = true,
+	window_close_confirmation = "NeverPrompt",
 
 	-- Terminal Appearance
-	--color_scheme = "Tokyo Night",
+	-- color_scheme = "Tokyo Night",
 	color_scheme = "Deafened (terminal.sexy)",
+	-- color_scheme = "Default (dark) (terminal.sexy)",
 	-- font = wezterm.font("JetBrainsMonoNL NF", { weight = "Regular", italic = false }),
-	-- font = wezterm.font("Victor Mono"),
-	-- font = wezterm.font("ZedMono NFM Extd"),
 	font = wezterm.font_with_fallback({ "ZedMono NFM Extd", "FiraCode Nerd Font Mono" }),
-	window_background_opacity = 0.85,
+	window_background_opacity = 1,
 	font_size = 12,
 	line_height = 1,
 	cell_width = 1,
+	colors = {
+		background = "#0f0f0f",
+		cursor_bg = "white",
+		cursor_border = "white",
+	},
 
 	-- Cursor Appearance
 	default_cursor_style = "BlinkingBar",
 	animation_fps = 1,
 	cursor_blink_ease_in = "Constant",
 	cursor_blink_ease_out = "Constant",
-	colors = {
-		cursor_bg = "white",
-		cursor_border = "white",
-	},
 
 	-- Window Appearance
 	window_decorations = "RESIZE", -- Removes the title bar but allows for window resizing
 	hide_tab_bar_if_only_one_tab = true,
+	enable_tab_bar = false,
 	window_padding = {
 		left = 0,
 		right = 0,
@@ -52,7 +54,7 @@ config = {
 	},
 
 	-- Miscellaneous Settings
-	max_fps = 120,
+	max_fps = 240,
 	prefer_egl = true,
 	enable_csi_u_key_encoding = true,
 }
