4ebf3b4a575b3703da2d3624086d6c0b31c2ff46
ToshoBread <93123377+ToshoBread@users.noreply.github.com>
2025-08-14 16:23:43 +0800

    revert: number and symbol swap

--- keybinds.lua
+++ keybinds.lua
@@ -24,29 +24,7 @@ local keybinds = {
 
 	{ key = "Enter", mods = "ALT", action = act.SendKey({ key = "Escape" }) },
 
-	-- Swap Number and Symbol Keys
-	{ key = "1", action = act.SendKey({ key = "!" }) },
-	{ key = "2", action = act.SendKey({ key = "@" }) },
-	{ key = "3", action = act.SendKey({ key = "#" }) },
-	{ key = "4", action = act.SendKey({ key = "$" }) },
-	{ key = "5", action = act.SendKey({ key = "%" }) },
-	{ key = "6", action = act.SendKey({ key = "^" }) },
-	{ key = "7", action = act.SendKey({ key = "&" }) },
-	{ key = "8", action = act.SendKey({ key = "*" }) },
-	{ key = "9", action = act.SendKey({ key = "(" }) },
-	{ key = "0", action = act.SendKey({ key = ")" }) },
-
-	{ key = "!", mods = "SHIFT", action = act.SendKey({ key = "1" }) },
-	{ key = "@", mods = "SHIFT", action = act.SendKey({ key = "2" }) },
-	{ key = "#", mods = "SHIFT", action = act.SendKey({ key = "3" }) },
-	{ key = "$", mods = "SHIFT", action = act.SendKey({ key = "4" }) },
-	{ key = "%", mods = "SHIFT", action = act.SendKey({ key = "5" }) },
-	{ key = "^", mods = "SHIFT", action = act.SendKey({ key = "6" }) },
-	{ key = "&", mods = "SHIFT", action = act.SendKey({ key = "7" }) },
-	{ key = "*", mods = "SHIFT", action = act.SendKey({ key = "8" }) },
-	{ key = "(", mods = "SHIFT", action = act.SendKey({ key = "9" }) },
-	{ key = ")", mods = "SHIFT", action = act.SendKey({ key = "0" }) },
-
+	-- Swap brackets and braces
 	{ key = "[", action = act.SendKey({ key = "{" }) },
 	{ key = "]", action = act.SendKey({ key = "}" }) },
 	{ key = "{", mods = "SHIFT", action = act.SendKey({ key = "[" }) },
